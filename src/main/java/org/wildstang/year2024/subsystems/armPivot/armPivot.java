5aa54de0e325ce191576ffe66940825c1c9a97c4
MoonPrograms <112990268+Sn4keProgr4ms@users.noreply.github.com>
2024-01-29 19:13:14 -0600

    Create armPivot.java

---
+++ src/main/java/org/wildstang/year2024/subsystems/armPivot/armPivot.java
@@ -0,0 +1,75 @@
+package org.wildstang.year2024.subsystems;
+
+import org.wildstang.framework.core.Core;
+import org.wildstang.framework.io.inputs.Input;
+import org.wildstang.framework.io.inputs.AnalogInput;
+import org.wildstang.framework.io.inputs.DigitalInput;
+import org.wildstang.framework.subsystems.Subsystem;
+import org.wildstang.hardware.roborio.outputs.WsSpark;
+import org.wildstang.year2024.robot.CANConstants;
+import org.wildstang.year2024.robot.WsInputs;
+import org.wildstang.year2024.robot.WsOutputs;
+import org.wildstang.year2024.robot.WsSubsystems;
+
+public class armPivot implements Subsystem{
+    /* 1. rename this ^ to be whatever your file is called
+     * 2. go to getName() and change the string "template" to a string that describes this subsystem
+     
+     * 3. add any variables you need under this comment block (inputs, outputs, doubles/booleans/ints)
+     * 4. Initialize any inputs and outputs under init()
+     * 5. add your code where it should be in inputUpdate() and update()
+     */
+
+     //example
+    //  private DigitalInput button;
+    //  private WsSpark motor;
+    
+    private WsSparkMax motor;
+    private AbsoluteEncoder absEncoder;
+
+    public Wrist(WsSparkMax outputMotor){
+        motor = outputMotor;
+        motor.setBrake();
+        absEncoder = motor.getController().getAbsoluteEncoder(Type.kDutyCycle);
+        absEncoder.setPositionConversionFactor(360.0);
+        absEncoder.setVelocityConversionFactor(360.0/60.0);
+        absEncoder.setInverted(SuperConts.TRUE_ENCODER_DIRECTION);
+        absEncoder.setZeroOffset(253.11);//310
+        motor.initClosedLoop(SuperConts.WRIST_P, SuperConts.WRIST_I, SuperConts.WRIST_D, 0, absEncoder, false);
+        motor.setCurrentLimit(15, 15, 0);
+    }
+    public double getPosition(){
+        return (absEncoder.getPosition())%360;
+    }
+    public void setPosition(double position){
+        motor.setPosition((position)%360);
+    }    
+    @Override
+    public void inputUpdate(Input source) {
+    }
+
+    @Override
+    public void init() {
+        //example
+        // button = (DigitalInput) WsInputs.DRIVER_DPAD_DOWN.get();
+        // button.addInputListener(this);
+        // motor = (WsSpark) WsOutputs.ANGLE1.get();
+    }
+
+    @Override
+    public void update() {
+        
+    }
+
+    @Override
+    public String getName() {
+        //
+        return "Arm Pivot";
+    }
+    @Override
+    public void resetState() {
+    }
+    @Override
+    public void selfTest() {
+    }
+}
