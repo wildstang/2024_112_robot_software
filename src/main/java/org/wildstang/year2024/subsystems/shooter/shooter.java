d7eb7bfcb62c674d09661d24855f52e93fe83531
Zaque2 <141452209+ZachHoegberg@users.noreply.github.com>
2024-02-13 17:52:38 -0600

    Minor updates for testing

--- src/main/java/org/wildstang/year2024/subsystems/shooter/shooter.java
+++ src/main/java/org/wildstang/year2024/subsystems/shooter/shooter.java
@@ -1,39 +1,36 @@
 package org.wildstang.year2024.subsystems.shooter;
 import org.wildstang.framework.core.Core;
-import org.wildstang.framework.io.inputs.AnalogInput;
 import org.wildstang.framework.io.inputs.DigitalInput;
 import org.wildstang.framework.io.inputs.Input;
 import org.wildstang.framework.subsystems.Subsystem;
 import org.wildstang.hardware.roborio.outputs.WsSpark;
-import org.wildstang.hardware.roborio.outputs.config.WsSparkConfig;
 import org.wildstang.year2024.robot.WsInputs;
 import org.wildstang.year2024.robot.WsOutputs;
 
-public class shooter implements Subsystem {
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Shooter implements Subsystem {
     private WsSpark shooterMotor1;
     private WsSpark shooterMotor2;
-    private double shooterMotor1Speed = 0.5;
-    private double shooterMotor2Speed = 0.5;
-    private AnalogInput leftTrigger;
+    private double shooterSpeed = 0.5;
+    private DigitalInput leftBumper;
     private DigitalInput dpadUp;
     private DigitalInput dpadDown;
-    private boolean leftTriggerPressed = false;
+    private boolean leftBumperPressed = false;
 
     @Override
     public void inputUpdate(Input source) {
-        if (leftTrigger.getValue() > 0.15){
-            leftTriggerPressed = true;
+        if (source == leftBumper && leftBumper.getValue()){
+            leftBumperPressed = true;
         }
         else{
-            leftTriggerPressed = false;
+            leftBumperPressed = false;
         }
-        if (dpadUp.getValue()){
-            shooterMotor1Speed += 0.05;
-            shooterMotor2Speed += 0.05;
+        if (source == dpadUp && dpadUp.getValue()){
+            shooterSpeed += 0.05;
         }
-        if (dpadDown.getValue()){
-            shooterMotor1Speed -= 0.05;
-            shooterMotor2Speed -= 0.05;
+        if (source == dpadDown && dpadDown.getValue()){
+            shooterSpeed -= 0.05;
         }
     }
 
@@ -41,8 +38,8 @@ public class shooter implements Subsystem {
     public void init() {
         shooterMotor1 = (WsSpark) Core.getOutputManager().getOutput(WsOutputs.SHOOTER1);
         shooterMotor2 = (WsSpark) Core.getOutputManager().getOutput(WsOutputs.SHOOTER2);
-        leftTrigger = (AnalogInput) Core.getInputManager().getInput(WsInputs.DRIVER_LEFT_TRIGGER);
-        leftTrigger.addInputListener(this);
+        leftBumper = (DigitalInput) Core.getInputManager().getInput(WsInputs.DRIVER_LEFT_SHOULDER);
+        leftBumper.addInputListener(this);
         //remember to add .addInputListener(this) for all inputs (including dpadUp and dpadDown)
         dpadUp = (DigitalInput) Core.getInputManager().getInput(WsInputs.DRIVER_DPAD_UP);
         dpadUp.addInputListener(this);
@@ -58,22 +55,22 @@ public class shooter implements Subsystem {
 
     @Override
     public void update() {
-        if (leftTriggerPressed){
-            shooterMotor1.setSpeed(shooterMotor1Speed);
-            shooterMotor2.setSpeed(shooterMotor2Speed);
+        if (leftBumperPressed){
+            shooterMotor1.setSpeed(shooterSpeed);
+            shooterMotor2.setSpeed(shooterSpeed);
         }
         //you can use else instead of this
         else{
             shooterMotor1.setSpeed(0);
             shooterMotor2.setSpeed(0);
         }
+        SmartDashboard.putNumber("shooter speed", shooterSpeed);
 
     }
     
     public void setShooterSpeed(double speed){
-        shooterMotor1Speed = speed;
-        shooterMotor2Speed = speed;
-        leftTriggerPressed = true;
+        shooterSpeed = speed;
+        leftBumperPressed = true;
     }
 
     @Override
