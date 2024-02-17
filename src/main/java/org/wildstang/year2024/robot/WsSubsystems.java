6db0eae563dd98df58aea934339073ed85354833
jpham1023 <phamthaoquyen@gmail.com>
2024-02-17 12:42:20 -0600

    added shooter in wsSubsystems

--- src/main/java/org/wildstang/year2024/robot/WsSubsystems.java
+++ src/main/java/org/wildstang/year2024/robot/WsSubsystems.java
@@ -1,6 +1,7 @@
 package org.wildstang.year2024.robot;
 
 import org.wildstang.framework.core.Subsystems;
+import org.wildstang.year2024.subsystems.shooter.ShooterSubsystem;
 import org.wildstang.year2024.subsystems.swerve.SwerveDrive;
 import org.wildstang.year2024.subsystems.targeting.WsVision;
 
@@ -12,7 +13,8 @@ public enum WsSubsystems implements Subsystems {
 
     // enumerate subsystems
     WS_VISION("Ws Vision", WsVision.class),
-    SWERVE_DRIVE("Swerve Drive", SwerveDrive.class)
+    SWERVE_DRIVE("Swerve Drive", SwerveDrive.class),
+    SHOOTER("Shoooter", ShooterSubsystem.class)
     ;
 
     /**
