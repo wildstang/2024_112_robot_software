54d604c9bfab859c8b36d712c8e5e4d873e55d4c
jpham1023 <phamthaoquyen@gmail.com>
2024-02-17 11:37:42 -0600

    created a setSpeed for motor and moved the file into a shooter folder

--- src/main/java/org/wildstang/year2024/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/org/wildstang/year2024/subsystems/shooter/ShooterSubsystem.java
@@ -66,27 +66,25 @@ public class ShooterSubsystem implements Subsystem{
             * ((distance - distanceMarks[indexes[0]]) / (distanceMarks[indexes[1]] - distanceMarks[indexes[0]])));
     }
 
-   /*  public double setAngle(double targetAngle){
-        //double currentAngle = shooterMotor.getPosition();
-        angleMotor.setPosition(targetAngle);
-
-
-
-
-    }*/
-
-
+   public void setShooterSpeed(boolean shootAllowed, double robotDistance){
+        if (shootAllowed){
+            motorSpeed = getSpeed(robotDistance);
+            motorAngle = getAngle(robotDistance);
+        }
+        else{
+           motorSpeed = 0;
+            motorAngle = 0; 
+        }
+   }
 
     @Override
     public void inputUpdate(Input source) {
         if(shootButton.getValue()){
             robotDistance = limelight.getDistanceFromAprilTag();
-            motorSpeed = getSpeed(robotDistance);
-            motorAngle = getAngle(robotDistance);
+            setShooterSpeed(true, robotDistance);
             
         }else if(!shootButton.getValue()){
-            motorSpeed = 0;
-            motorAngle = 0;
+            setShooterSpeed(false,robotDistance);
         }
     }
 
