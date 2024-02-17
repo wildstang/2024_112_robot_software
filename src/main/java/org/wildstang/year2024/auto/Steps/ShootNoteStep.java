4bbb563f0c8c02163092ec6c1e8f452099cd8bc2
jpham1023 <phamthaoquyen@gmail.com>
2024-02-17 14:24:14 -0600

    added shooter step(incomplete)

---
+++ src/main/java/org/wildstang/year2024/auto/Steps/ShootNoteStep.java
@@ -0,0 +1,32 @@
+package org.wildstang.year2024.auto.Steps;
+
+import org.wildstang.framework.auto.AutoStep;
+import org.wildstang.framework.core.Core;
+import org.wildstang.year2024.robot.WsSubsystems;
+
+
+public class ShootNoteStep extends AutoStep {
+    private boolean shootPossible;
+
+    public ShootNoteStep(boolean shootPossible){
+        this.shootPossible = shootPossible;
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public String toString() {
+        return "Shoot Note step";
+    }
+    
+}
